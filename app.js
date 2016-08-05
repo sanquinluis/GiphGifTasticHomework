1049ad1d4148e4a40fddc2e40cd19fbb5f150548
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-05 03:06:31 -0400

    thebuttons

--- app.js
+++ app.js
@@ -8,29 +8,26 @@ $(document).ready(function(){
     var api = 'http://api.giphy.com/v1/gifs/search?q=';
     var apiKey = '&api_key=dc6zaTOxFJmzC';
     var limit = '&limit=10&q=';	
-    var urlCombo = api + apiKey + limit + valueInput;
     var valueInput = $('#gyphy-input').val().trim();
-      	
-        $('.theCountry').on('click', function(){
-	 	     theCountry.push(valueInput);
-             valueInput = select(theCountry);
-    });
-        function alertName(){
-        var countryName = $(this).data('name');
-        alert(countryName);
+    var urlCombo = api + apiKey + limit + valueInput;
+    
+       
+        function tellName(){
+        var countryName = $(this).data('data-name');
+        
     }
          $('.rightIn').empty();
        
-        $.ajax({url: urlCombo , 
-        	method: 'GET'
+        $.ajax({url: urlCombo, 
+        	    method: 'GET'
         })
           .done(function(response) {
-			$('.theButton').html(JSON.stringify(response));	
+			$('theCountry').html(JSON.stringify(response));	
 
                 var results = response.data;
                 for (var i = 0; i < results.length; i++) {
     		};
-    		$('.theCountry').empty();
+    
                 // console.log(value);
     			console.log(response);
                 console.log(valueInput);
@@ -38,17 +35,34 @@ $(document).ready(function(){
     		 	$('.images').append(valueInput);
     		 	$('.images').html(response.urlCombo);
     	});
-    	//This function adds the buttons that are associated with the var theCountry.
-    		$('#theButton').html
-    			for (var x = 0; x < theCountry.length; x++){
-			    		var b = $('<button>').append(theCountry).css('margin', '5px');
-			    		b.addClass('btn btn-info country')
-			    		b.attr('data-name', theCountry[x]);
-			    		b.text(theCountry[x]);
-			    		$('.rightIn').append(b);
-	    	};
-         
-            $(document).on('click', '.countryName', alertName);
+    	//This function "adds" the buttons 
+    		function renderButtons(){ 
+
+        // YOUR CODE GOES HERE
+        $(".rightIn").empty();
+
+        $.each(theCountry,function(index,element){
+            var newButton = $("<button/>").addClass( "btn btn-info country").attr('name',element).text(element).css({'margin': '5px'});
+            $(".rightIn").append(newButton);
+        });
+
+    }
+
+    // This function handles events where one button is clicked
+    $('#theButton').on('click', function(){
+            //this adds the new movie to the list of movies
+            theCountry.push($("#gyphy-input").val());
+        
+        renderButtons()
+        $("#gyphy-input").val("");
+        return false;
+    })
+
+
+    // This allows renderButtons() to run and creat the origional buttons
+    renderButtons();
+
+            
     		console.log(urlCombo);
             return false;
 });
