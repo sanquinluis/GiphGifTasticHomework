867ac1a6d19a970085d3d08a5d7e0f1aba03c670
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-05 21:16:06 -0400

    Gif

--- app.js
+++ app.js
@@ -1,70 +1,76 @@
 $(document).ready(function(){
 
+	
+    var api = 'http://api.giphy.com/v1/gifs/search?q=';
+    var apiKey = '&api_key=dc6zaTOxFJmzC';
 
-	var theCountry = ['Canada', 'United States', 'Mexico', 'Guatemala', 'Belize', 'Cuba', 
+    var theCountry = ['Canada', 'United States', 'Mexico', 'Guatemala', 'Belize', 'Cuba', 
     'Honduras', 'El Salvador', 'Nicaraguas', 'Costa Rica', 'Panama', 'Jamaica','Haiti',
     'Dominican Republic', 'Puerto Rico'];
 
-    var api = 'http://api.giphy.com/v1/gifs/search?q=';
-    var apiKey = '&api_key=dc6zaTOxFJmzC';
     var limit = '&limit=10&q=';	
     var valueInput = $('#gyphy-input').val().trim();
-    var urlCombo = api + apiKey + limit + valueInput;
+    var urlCombo = api + valueInput + apiKey + limit;
     
-       
-        function tellName(){
-        var countryName = $(this).data('data-name');
-        
-    }
-         $('.rightIn').empty();
+    //function=====  
+        $('button').on('Click',function(){
+            var countryName = $(this).data("info");
+            urlCombo();
+    });
+        $('.rightIn').empty();
        
         $.ajax({url: urlCombo, 
-        	    method: 'GET'
-        })
-          .done(function(response) {
-			$('theCountry').html(JSON.stringify(response));	
 
-                var results = response.data;
-                for (var i = 0; i < results.length; i++) {
+        	method: 'GET'
+        
+        })
+        //Function that creates a div and puts the Image.  
+        .done(function(response) {
+	       
+            //
+            var theGif = response.data;
+            for (var i = 0; i < theGif.length; i++) {
+                var newDivGif = $('<div class="gifImg">');
+
+                var rating = $('<p>').text("Rating: " + theGif[i].rating);
+
+                var theImages = $('<img>')
+                    .addClass('bandimg')
+                    .attr('src', theGif[j].images.fixed_height_still.url)
+                    .attr('data-still', theGif[j].images.fixed_height_still.url)
+                    .attr('data-animate', theGif[j].images.fixed_height.url)
+                    .attr('data-state', 'still');
+
+                newDivGif.prepend(rating);
+                newDivGif.prepend(theImages);
+
     		};
-    
-                // console.log(value);
-    			console.log(response);
-                console.log(valueInput);
-    		 	console.log(results);
-    		 	$('.images').append(valueInput);
-    		 	$('.images').html(response.urlCombo);
+            
+            console.log(urlCombo);
+    		console.log(response);
+            console.log(valueInput);
+    		
+           
     	});
     	//This function "adds" the buttons 
-    		function renderButtons(){ 
-
-        // YOUR CODE GOES HERE
-        $(".rightIn").empty();
-
-        $.each(theCountry,function(index,element){
+    	function renderButtons(){ 
+            $(".rightIn").empty();
+            $.each(theCountry,function(index,element){
             var newButton = $("<button/>").addClass( "btn btn-info country").attr('name',element).text(element).css({'margin': '5px'});
             $(".rightIn").append(newButton);
         });
 
     }
-
-    // This function handles events where one button is clicked
-    $('#theButton').on('click', function(){
-            //this adds the new movie to the list of movies
+        // This handels the event when click
+        $('#theButton').on('click', function(){
+            //adds the new country
             theCountry.push($("#gyphy-input").val());
-        
-        renderButtons()
+            renderButtons()
         $("#gyphy-input").val("");
-        return false;
-    })
-
-
-    // This allows renderButtons() to run and creat the origional buttons
-    renderButtons();
-
-            
-    		console.log(urlCombo);
             return false;
+    })
+            renderButtons();
+            
 });
 
 
