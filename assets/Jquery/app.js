872ab6a7347f035efa767bd0ab0bbb829c105823
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-06 22:36:25 -0400

    update

--- assets/Jquery/app.js
+++ assets/Jquery/app.js
@@ -1,71 +1,73 @@
 $(document).ready(function(){
 
-	//Universal variables that access the api giphy source.
+    //Universal variables that access the api giphy source.
     var api = 'https://api.giphy.com/v1/gifs/search?q=';
     //api key that authorize the use of the the web-page.
     var apiKey = '&api_key=dc6zaTOxFJmzC';
     //this is my array of countries that are push to the <div> with the class of .rightIn.
-    var theCountry = ['Canada', 'United States', 'Mexico', 'Guatemala', 'Belize', 'Cuba', 
+    var theCountry = ['Canada', 'United States', 'Mexico', 'Guatemala', 'Belize', 'Cuba',
     'Honduras', 'El Salvador', 'Nicaraguas', 'Costa Rica', 'Panama', 'Jamaica','Haiti',
     'Dominican Republic', 'Puerto Rico'];
     //This var is limiting the number of giphy shown on the screen.
-    var limit = '&limit=10&q=';	
+    var limit = '&limit=10&q=';
     var valueInput = $('#gyphy-input').val().trim();
-
+    //This adds all the access point
     var urlCombo = api + valueInput + apiKey + limit;
     //=============================================================================
-    //First function that controls the buttons that are inside the array and the on 
+    //First function that controls the buttons that are inside the array and the on
        $(document.body).on('click', '.country', function() {
             var countryName = $(this).attr('name');
             $(".hereTheyare").empty();
-   
-        $.ajax({url: urlCombo + countryName, 
 
-        	method: 'GET'
+        $.ajax({url: urlCombo + countryName,
+
+            method: 'GET'
         })
 
-        //Function that creates a div and puts the Image.  
+        //Function that creates a div and puts the Image.
         .done(function(response) {
-	       
-            //
+
+            //This for loop is going inside the new created giphys 
             var theGif = response.data;
             for (var i = 0; i < theGif.length; i++) {
 
                 var newDivGif = $('<div>');
 
-                var rating = $('<p>').text("Rating: " + theGif[i].Rating);
-
-                var theImages = $('<img>').css({'margin':'5px','width': '200px', 'height':'200px','float':'left'})
+                var rating = $('<p>').text("Rated: " + theGif[i].rating);
+                // .css({'margin':'5px','width': '200px', 'height':'200px','float':'left'})
+                var theImages = $('<img>')
                     .addClass('gifyIm')
                     .attr('src', theGif[i].images.fixed_height_still.url)
                     .attr('data-still', theGif[i].images.fixed_height_still.url)
                     .attr('data-animate', theGif[i].images.fixed_height.url)
                     .attr('data-state', 'still');
 
-                newDivGif.prepend(rating);
-                newDivGif.prepend(theImages);
-                $('.hereTheyare').append(theImages);
-    		};
+                newDivGif.append(rating);
+                newDivGif.append(theImages);
+                $('.hereTheyare').append(newDivGif);
+                $('.hereTheyare').append(newDivGif);
+            }
                 console.log(response);
                 console.log(rating);
         });
-            //animating and stoping the gif??
-            $('.gifyIm').on('click', function(){
-                var state = $(gifyIm).attr('data-state'); 
+    });
+            //animating and stopping the giphy
+            $(document).on('click','.gifyIm', function(){
+                var state = $(this).attr('data-state');
                 if (state == 'still') {
-                            $(gifyIm).attr('src', $(this).data('animate'));
-                            $(gifyIm).attr('data-state', 'animate');
-            }   else {
-                            $(gifyIm).attr('src', $(this).data('still'));
-                            $(gifyIm).attr('data-state', 'still');
-                    }   
-            }); 
+                            $(this).attr('src', $(this).data('animate'));
+                            $(this).attr('data-state', 'animate');
+                 }  else {
+                            $(this).attr('src', $(this).data('still'));
+                            $(this).attr('data-state', 'still');
+                    }
+            })
             console.log(urlCombo);
             console.log(valueInput);
-            console.log();
-    	});
-    	//This function "adds" the buttons 
-    	function renderButtons(){ 
+            // console.log();
+
+        //This function "adds" the buttons
+        function renderButtons(){
             $(".rightIn").empty();
             $.each(theCountry,function(index,element){
             var newButton = $("<button/>").addClass( "btn btn-info country").attr('name',element).text(element).css({'margin': '5px'});
@@ -84,10 +86,5 @@ $(document).ready(function(){
     })
 
             renderButtons();
-            
+
 });
-
-
-
-
-
