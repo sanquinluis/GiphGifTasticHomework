431daa01059ce76e600f8eb3ec1875d72f1c66e6
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-08-06 21:05:53 -0400

    update

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
 
-	
+	//Universal variables that access the api giphy source.
     var api = 'https://api.giphy.com/v1/gifs/search?q=';
+    //api key that authorize the use of the the web-page.
     var apiKey = '&api_key=dc6zaTOxFJmzC';
-
+    //this is my array of countries that are push to the <div> with the class of .rightIn.
     var theCountry = ['Canada', 'United States', 'Mexico', 'Guatemala', 'Belize', 'Cuba', 
     'Honduras', 'El Salvador', 'Nicaraguas', 'Costa Rica', 'Panama', 'Jamaica','Haiti',
     'Dominican Republic', 'Puerto Rico'];
-
+    //This var is limiting the number of giphy shown on the screen.
     var limit = '&limit=10&q=';	
     var valueInput = $('#gyphy-input').val().trim();
+
     var urlCombo = api + valueInput + apiKey + limit;
-    
-    //function that connects  
+    //=============================================================================
+    //First function that controls the buttons that are inside the array and the on 
        $(document.body).on('click', '.country', function() {
             var countryName = $(this).attr('name');
             $(".hereTheyare").empty();
@@ -29,12 +31,12 @@ $(document).ready(function(){
             var theGif = response.data;
             for (var i = 0; i < theGif.length; i++) {
 
-                var newDivGif = $('<div class="gifImg">');
+                var newDivGif = $('<div>');
 
-                var rating = $('<p>').text("Rating: " + theGif[i].rating);
+                var rating = $('<p>').text("Rating: " + theGif[i].Rating);
 
-                var theImages = $('<img>').css({'margin':'5px','width': '200px', 'height':'200px'})
-                    .addClass('gifyIm animate still')
+                var theImages = $('<img>').css({'margin':'5px','width': '200px', 'height':'200px','float':'left'})
+                    .addClass('gifyIm')
                     .attr('src', theGif[i].images.fixed_height_still.url)
                     .attr('data-still', theGif[i].images.fixed_height_still.url)
                     .attr('data-animate', theGif[i].images.fixed_height.url)
@@ -45,20 +47,22 @@ $(document).ready(function(){
                 $('.hereTheyare').append(theImages);
     		};
                 console.log(response);
+                console.log(rating);
         });
-            //animating the gif??
+            //animating and stoping the gif??
             $('.gifyIm').on('click', function(){
-                var state = $(this).attr('data-state'); 
+                var state = $(gifyIm).attr('data-state'); 
                 if (state == 'still') {
-                            $(this).attr('src', $(this).data('animate'));
-                            $(this).attr('data-state', 'animate');
+                            $(gifyIm).attr('src', $(this).data('animate'));
+                            $(gifyIm).attr('data-state', 'animate');
             }   else {
-                            $(this).attr('src', $(this).data('still'));
-                            $(this).attr('data-state', 'still');
+                            $(gifyIm).attr('src', $(this).data('still'));
+                            $(gifyIm).attr('data-state', 'still');
                     }   
             }); 
             console.log(urlCombo);
             console.log(valueInput);
+            console.log();
     	});
     	//This function "adds" the buttons 
     	function renderButtons(){ 
